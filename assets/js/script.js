094d2268b9b2977001658da42cd3ca0f473b775a
Dimitris Giannoulis <dimitrisgiannoulis.jg@gmail.com>
2023-11-12 16:47:52 +0000

    Organizes script.js better@

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
-// day.js extensions
+// EXTENSIONS (for day.js) //
+
 dayjs.extend(window.dayjs_plugin_calendar);
 dayjs.extend(window.dayjs_plugin_advancedFormat);
+
+
 
 
 // GLOBAL SELECTORS / CONSTANTS //
@@ -10,6 +13,9 @@ const timeEl = $('#currentTime');
 const scheduleSectionDiv = $('.schedule-section');
 const START_TIME = 9;
 const END_TIME = 17;
+
+
+
 
 // FUNCTIONS //
 
@@ -25,12 +31,7 @@ function updateTime() {
         } ,1000);
 }
 
-
-
-
-
-// CREATE BLOCKS
-// Figure out how to make a block for every hour from 9 to 5 inclusive
+// Creates hour-block
 function createHourBlock(h) {
     $(document).ready(function() {
 
@@ -40,7 +41,7 @@ function createHourBlock(h) {
         let currentHour = dayjs().hour();
 
         // Creates hour block
-        let hourBlock = $('<div class="row hour-block">');
+        let hourBlock = $(`<div class="row hour-block hour-block-${h}">`);
     
         // Creates hour column and appends the current hour
         let hourCol = $('<div class="col-3 col-md-2 hour">');
@@ -71,44 +72,6 @@ function createHourBlock(h) {
 }
 
 
-    // each block should: 
-    // allow for text entry (textarea or input element)
-    // include an hour label
-    // include a save button that
-        // has a click handler .on('click') that writes to local storage
-    // be styled based on whether the hour is in the past, presest, or future
-
-    // each block should add the stored notes to the appropriate
-
-
-
-// Figure out how to make a block for every hour from 9 am to 5 pm inclusive
-//   each block should allow for text entry (perhaps the block is an input element/textarea)
-//   based on hour of the day, 
-//      each block should be styled based on whether the
-//          hour is in the past, present, or future
-//      any block in the past should be locked such that
-//          text cannot be edited (Nice to have)
-//     
-//   each block should include an hour label
-//   each block should include a save button
-//      each save button should have a click handler that
-//         writes to local storage - somehow, we need to associate each hour's tasks
-//         with a separate key
-//         Maybe save a single object for the page where the keys in the object are hour numbers and the values are the tasks
-//             -OR-
-//         Maybe save each task separately to local storage, e.g.
-//               localStorage.setItem("9 am", "eat some pie");
-//               localStorage.setItem("10 am", "pretend to work")
-//   each block should load stored tasks for the given hour from local storage
-
-
-// Nice to have: Include reminder functionality
-//  As the current time approaches the next hour, trigger
-//  an alert/dialog/modal that will say what is upcoming
-// Nice to have: Some way to indicate a task has been completed (checkbox, button, etc.?)
-
-
 
 
 // ## STARTUP FUNCTION ## //
@@ -128,6 +91,3 @@ function startApp () {
 
 // Calls startApp() function that starts all other startup functions
 startApp()
-
-let hour = dayjs().hour(9);
-console.log(hour.format('h A'))
