2f2fec90bbfcf2e5ef3b0355e24fb9f9a4785e9b
Dimitris Giannoulis <dimitrisgiannoulis.jg@gmail.com>
2023-11-12 17:27:50 +0000

    Stores text values to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,13 +6,13 @@ dayjs.extend(window.dayjs_plugin_advancedFormat);
 
 
 
-// GLOBAL SELECTORS / CONSTANTS //
+// GLOBAL SELECTORS - CONSTANTS //
 
-const currentDay = dayjs();
 const timeEl = $('#currentTime');
 const scheduleSectionDiv = $('.schedule-section');
+const CURRENT_DATE = dayjs();
 const START_TIME = 9;
-const END_TIME = 17;
+const END_TIME = 20;
 
 
 
@@ -22,7 +22,7 @@ const END_TIME = 17;
 // Updates time every minute
 function updateTime() {
     // sets current time to timeEl
-    timeEl.text(dayjs().format('h:mm:ss A'));
+    timeEl.text(CURRENT_DATE.format('h:mm:ss A'));
 
     // Updates it every second
     setInterval( function() {
@@ -38,17 +38,20 @@ function createHourBlock(h) {
         // Get hour from day.js
         let hour = dayjs().hour(h);
         // Get current hour
-        let currentHour = dayjs().hour();
+        let currentHour = CURRENT_DATE.hour();
 
         // Creates hour block
-        let hourBlock = $(`<div class="row hour-block hour-block-${h}">`);
+        let hourBlock = $(`<div class="row hour-block">`);
+        hourBlock.attr('id', `hour-block-${h}`)
     
         // Creates hour column and appends the current hour
         let hourCol = $('<div class="col-3 col-md-2 hour">');
         hourCol.append(`<div class=${h}>${hour.format('h A')}</div>`);
-    
+        hourCol.attr('id', `hour-col-${h}`)
+
         // Creates text column
         let textCol = $('<textarea class="col form-control description" name="description-text" id="textArea">');
+        textCol.attr('id', `text-col-${h}`)
 
         // Add appropriate class depending on hour of the day
         if (h === currentHour) {
@@ -60,8 +63,9 @@ function createHourBlock(h) {
         }
         
         //  Creates Save button and appends icon
-        let saveButton = $('<button type="button" class=" btn d-flex justify-content-center align-items-center btn-danger col-2 col-md-1 saveBtn">');
-        saveButton.append('<i class="fas fa-save"></i>');
+        let saveButton = $('<button type="button" class=" fas fa-save btn d-flex justify-content-center align-items-center btn-danger col-2 col-md-1 saveBtn">');
+        // saveButton.append('<i class="fas fa-save"></i>');
+        saveButton.attr('id', `save-button-${h}`)
         
         // Appends all columns to hourBlock
         hourBlock.append(hourCol, textCol, saveButton);
@@ -78,8 +82,8 @@ function createHourBlock(h) {
 
 function startApp () {
     // 1. Figure out today'sdate, and put it at the top of the page
-    let dayEl = $('#currentDay');
-    dayEl.text(currentDay.format('dddd, MMMM Do'));
+    let dayEl = $('#CURRENT_DATE');
+    dayEl.text(CURRENT_DATE.format('dddd, MMMM Do'));
     // Starts Timer
     updateTime()
 
@@ -91,3 +95,25 @@ function startApp () {
 
 // Calls startApp() function that starts all other startup functions
 startApp()
+
+scheduleSectionDiv.on('click', function(e) {
+    console.log(e.target);
+    console.log(e.target.parentElement);
+
+    const target = e.target;
+    if (target.tagName === 'BUTTON') {
+        // Store hour-block's hour in a variable
+        let blockText = target.previousElementSibling.value;
+        let blockHour = target.previousElementSibling.previousElementSibling.innerText;
+        console.log(`${blockHour}: ${blockText}`);
+        storeText(blockHour, blockText);
+    }
+});
+
+function storeText(key, value) {
+    if (value !== "") {
+        localStorage.setItem(key, value);
+    } else {
+        localStorage.removeItem(key);
+    }
+}
