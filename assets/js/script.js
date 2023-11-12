b6630e22988c8553d0950923a2c2d94664f2ed4a
Dimitris Giannoulis <dimitrisgiannoulis.jg@gmail.com>
2023-11-12 19:47:50 +0000

    Improves project's comments and organization

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,43 @@
-// EXTENSIONS (for day.js) //
+/* WORK DAY SCHEDULER
 
+This project uses jQuery and day.js to display
+a calendar-like scheduler that stores user's preferencees
+to localStorage.
+
+*/
+
+
+
+// EXTENSIONS (for day.js) //
 dayjs.extend(window.dayjs_plugin_calendar);
 dayjs.extend(window.dayjs_plugin_advancedFormat);
 
-
-
-
-// GLOBAL SELECTORS - CONSTANTS //
-
+// GLOBAL SELECTORS
 const timeEl = $('#currentTime');
 const scheduleSectionDiv = $('.schedule-section');
-const CURRENT_DATE = dayjs();
+
+// GLOBAL CONSTANTS
+const CURRENT_DATE = dayjs('2023-11-12 13:30:00'); // For testing
+// const CURRENT_DATE = dayjs();
 const dateFormated = `${CURRENT_DATE.format('YYMMDD')}`
 const START_TIME = 9;
-const END_TIME = 20;
+const END_TIME = 17;
 const userData = JSON.parse(localStorage.getItem('userData')) || {};
-console.log(userData);
 
-let user = {
-    lastSaved: dateFormated,
-    schedule: {}
-};
+// GLOBAL VARIABLES //
+let hasData = false;
+let user = { lastSaved: dateFormated, schedule: {} };
 
 
+// ========= //
+// Starts all startup functions (declared below)
+startApp();
+// ========= //
 
-// FUNCTIONS //
+
+
+
+// ## FUNCTIONS ## //
 
 // Updates time every minute
 function updateTime() {
@@ -32,25 +45,42 @@ function updateTime() {
     timeEl.text(CURRENT_DATE.format('h:mm:ss A'));
 
     // Updates it every second
-    setInterval( function() {
-            let currentTime = dayjs();
-            timeEl.text(currentTime.format('h:mm:ss A'));
-        } ,1000);
+    setInterval(function () {
+        let currentTime = dayjs();
+        timeEl.text(currentTime.format('h:mm:ss A'));
+    }, 1000);
 }
 
-// Creates hour-block
-function createHourBlock(h) {
-    $(document).ready(function() {
 
-        // Get hour from day.js
+
+// Updates 'hasData' and 'user[schedule]' based on userData
+function updateUserData() {
+    if (Object.keys(userData).length > 0) {
+        if (userData['lastSaved'] === dateFormated && Object.keys(userData['schedule']).length > 0) {
+            hasData = true;
+            user.schedule = userData.schedule;
+        } else {
+            // Resets every day
+            localStorage.removeItem('userData');
+        }
+    }
+}
+
+
+
+// Creates hour-block instance
+function createHourBlock(h) {
+    $(document).ready(function () {
+
+        // Gets hour from day.js
         let hour = dayjs().hour(h);
-        // Get current hour
+        // Gets current hour
         let currentHour = CURRENT_DATE.hour();
 
         // Creates hour block
         let hourBlock = $(`<div class="row hour-block">`);
         hourBlock.attr('id', h)
-    
+
         // Creates hour column and appends the current hour
         let hourCol = $('<div class="col-3 col-md-2 hour">');
         hourCol.append(`<div class=${h}>${hour.format('h A')}</div>`);
@@ -60,7 +90,7 @@ function createHourBlock(h) {
         let textCol = $('<textarea class="col form-control description" name="description-text" id="textArea">');
         textCol.attr('id', `text-col-${h}`)
 
-        // Add appropriate class depending on hour of the day
+        // Adds appropriate class depending on hour of the day
         if (h === currentHour) {
             textCol.addClass('present');
         } else if (h < currentHour) {
@@ -68,15 +98,21 @@ function createHourBlock(h) {
         } else {
             textCol.addClass('future');
         }
-        
+
+        if (hasData) {
+            if (userData['schedule'].hasOwnProperty(h)) {
+                textCol.text(userData['schedule'][h]);
+            }
+        }
+
         //  Creates Save button and appends icon
         let saveButton = $('<button type="button" class=" fas fa-save btn d-flex justify-content-center align-items-center btn-danger col-2 col-md-1 saveBtn">');
         // saveButton.append('<i class="fas fa-save"></i>');
         saveButton.attr('id', `save-button-${h}`)
-        
+
         // Appends all columns to hourBlock
         hourBlock.append(hourCol, textCol, saveButton);
-    
+
         // Appends all hourBlock to shcedule div
         scheduleSectionDiv.append(hourBlock);
     });
@@ -87,23 +123,28 @@ function createHourBlock(h) {
 
 // ## STARTUP FUNCTION ## //
 
-function startApp () {
-    // 1. Figure out today'sdate, and put it at the top of the page
+function startApp() {
+    // 1. Figures out today's date and display it at the top of the page
     let dayEl = $('#CURRENT_DATE');
     dayEl.text(CURRENT_DATE.format('dddd, MMMM Do'));
-    // Starts Timer
-    updateTime()
 
-    
+    // 2. Starts Timer
+    updateTime();
+
+    // 3. Updates 'user' and 'hasData' global vars depending on userData
+    updateUserData();
+
+    // 4. Creates hour blocks based on START and END global constants, declared on top
     for (let i = START_TIME; i <= END_TIME; i++) {
         createHourBlock(i);
     }
 }
 
-// Calls startApp() function that starts all other startup functions
-startApp()
 
-scheduleSectionDiv.on('click', function(e) {
+
+// ## EVENT HANDLERS ## //
+
+scheduleSectionDiv.on('click', function (e) {
     console.log(e.target);
     console.log(e.target.parentElement);
 
@@ -127,6 +168,7 @@ function storeText(key, value) {
     } else {
         delete schedule[key];
         console.log(user);
+        console.log('HERE!')
     }
 
     userJSON = JSON.stringify(user);
